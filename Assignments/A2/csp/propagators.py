fd7cf8592d9afbd69fbf5d8398826b5f64b1a659
RichardMac <richkejie@gmail.com>
2024-02-22 21:10:19 -0500

    [A2] Finished writing functions for propagators.py

--- Assignments/A2/csp/propagators.py
+++ Assignments/A2/csp/propagators.py
@@ -78,7 +78,110 @@ def prop_FC(csp, newVar=None):
        track of all pruned variable,value pairs and return '''
     #IMPLEMENT
 
+    if newVar != None:
+        # if newVar is given, check all constraints with newVar in their scope
+        constraints = csp.get_cons_with_var(newVar)
+    else:
+        # if newVar not given, check all constraints
+        constraints = csp.get_all_cons()
+
+    # keep track of [(Variable, Value), (Variable, Value) ...]
+    # where (Variable, Value) represents Value having been pruned
+    # from Variable's domain by prop_FC
+    pruned = []
+
+    def check(cons, var):
+        '''Check that all values in variable var's current domain satisfy
+           the constraint cons. If a value does not, prune it, and record
+           it in the list pruned. Return True if DWO does not occur, False
+           otherwise.'''
+        
+        for d in var.cur_domain():
+            # assign d to var
+            var.assign(d)
+            # check if this new assignment satisfies cons
+            asgns = []
+            for v in cons.get_scope():
+                asgns.append(v.get_assigned_value())
+            # if it does not, prune d from var's domain
+            # and record in list pruned
+            if cons.check(asgns) == False:
+                var.prune_value(d)
+                pruned.append((var, d))
+            # unassign d from var, and check next value in var's domain
+            var.unassign()
+        
+        # if DWO occurred, return False
+        if var.cur_domain_size() == 0:
+            return False
+        # otherwise, return True
+        return True
+
+    # we are only asked to forward check unary constraints
+    # unary constraints are those with a "scope of just one variable"
+    #   this includes constraints with a multi-variable scope but only
+    #   one variable in the scope is unassigned
+    # this is not quite the same as the FC algorithm from lecture
+    for cons in constraints:
+        if cons.get_n_unasgn() == 1:
+            var = cons.get_unasgn_vars()[0] 
+                # this is the singular variable that is not assigned
+            if check(cons, var) == False:
+                # DWO occurred, so there is no supporting assignment
+                # bt_search must backtrack
+                return False, pruned
+        
+    # pruning is finished and no DWO occurred
+    return True, pruned
+    
+
 def prop_FI(csp, newVar=None):
     '''Do full inference. If newVar is None we initialize the queue
        with all variables.'''
     #IMPLEMENT
+    
+    if newVar != None:
+        # if newVar is given, initialize queue with just newVar
+        queue = [newVar]
+    else:
+        # if newVar is not given, initialize queue with all variables
+        queue = csp.get_all_vars()
+
+    # keep track of [(Variable, Value), (Variable, Value) ...]
+    # where (Variable, Value) represents Value having been pruned
+    # from Variable's domain by prop_FI
+    pruned = []
+
+    # run full inferencing:
+    while len(queue) > 0:
+        # pop first variable in queue
+        w = queue.pop(0)
+        # iterate over constraints with w in their scope
+        for cons in csp.get_cons_with_var(w):
+            # iterate over variables in scope of cons, excluding w
+            for v in cons.get_scope():
+                if v == w:
+                    continue
+                # make copy of v's current domain
+                S = v.cur_domain()
+                # iterate over v's current domain
+                for d in v.cur_domain():
+                    # if a supporting assignment cannot be found for v = d
+                    # prune d from v's current domain and record in list pruned
+                    # if DWO occurred, return False
+                    if not cons.has_support(v, d):
+                        v.prune_value(d)
+                        pruned.append((v, d))
+                        # if DWO occurred, return False
+                        if v.cur_domain_size() == 0:
+                            queue.clear()
+                            return False, pruned
+                # if DWO did not occur, check if any values were
+                # pruned from v's domain, if so, add v back to queue
+                if v.cur_domain() != S:
+                    queue.append(v)
+
+    # FI finishes without DWO, return True
+    return True, pruned
+
+
